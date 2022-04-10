9f5522313d7768114cd675d5d8322a6b01093723
onur <sevgilionur25@hotmail.com>
2022-04-10 20:19:33 +0300

    added .gitignore file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,10 @@ public class HelloWorld {
     //Doing awesome work 3!
     //Awesome work 3 is done and ready to be committed
     //and ready to be pushed to remote repository
-    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
 }
